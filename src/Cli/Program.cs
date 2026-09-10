37f80e9a9afbce296ecce58b4abf9aa80fcd1a3f
yorleX23 <bogdandzikovskiy@gmail.com>
2026-09-10 23:35:23 +0300

    lab01: solution CrossApp, Cli, вибір домену

---
+++ src/Cli/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("CrossApp - практикум з крос-платформного програмування");
+Console.WriteLine("Студент: Дзіковський Богдан, група ФЕІ-37");
+Console.WriteLine(new string('-', 52));
+Console.WriteLine($"OC (OSDescription): {RuntimeInformation.OSDescription}");
+Console.WriteLine($"OC (Environment)  : {Environment.OSVersion}");
+Console.WriteLine($"Архітектура процесу: {RuntimeInformation.ProcessArchitecture}");
+Console.WriteLine($"Версія .NET (CLR) : {Environment.Version}");
+Console.WriteLine($"Runtime           : {RuntimeInformation.FrameworkDescription}");
+Console.WriteLine($"Каталог застосунку: {AppContext.BaseDirectory}");
+Console.WriteLine($"Поточний каталог  : {Environment.CurrentDirectory}");
+Console.WriteLine(new string('-', 52));
+Console.WriteLine("Предметна область: Замовлення (Customer, Product, Order, OrderLine)");
